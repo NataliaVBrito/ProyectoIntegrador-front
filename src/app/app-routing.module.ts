84d6b2be8de8b71ca6fdb3580841ffc651879986
Natalia Brito <natbrito18rgmail.com>
2022-05-23 21:35:30 -0300

    Corrección de errores y llenado de casillas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './components/header/header.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+
+
+
+const routes: Routes = [
+  {path: 'inicio', component: HeaderComponent},
+  { path: 'sobre-mi', component: SobreMiComponent },
+  { path: 'educacion', component: EducacionComponent },
+  { path: 'tecnologias', component: TecnologiasComponent },
+  { path: 'proyectos', component: ProyectosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  {path: '**', redirectTo: 'inicio', pathMatch: 'full'}/*,
+  {path: '**', component: Pagina404Component}*/
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+   CommonModule,
+   RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
-import { EducacionComponent } from './components/educacion/educacion.component';
-import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
-import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     NavbarComponent,
-    BannerComponent,
+    HeaderComponent,
     SobreMiComponent,
     EducacionComponent,
     TecnologiasComponent,
@@ -28,9 +30,11 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ RouterModule]
 })
 export class AppModule { }
